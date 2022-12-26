baac21b7f13b3b222ad29c683ad451de35f836d1
rk <48819681+rksan@users.noreply.github.com>
2022-12-26 17:13:00 +0900

    enable vue.js devtools

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
 
 import { createPinia } from "pinia";
 
 import { BootstrapVue3 } from "bootstrap-vue-3";
-
-const pinia = createPinia();
 
 // main.js/ts
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-createApp(App).use(pinia).use(BootstrapVue3).mount("#app");
+const pinia = createPinia();
+const app = createApp(App);
+
+app.config.devtools = true;
+
+app.use(pinia).use(BootstrapVue3).mount("#app");
