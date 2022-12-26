12d154b4aac12d916d2bed055022e17c2e01bf88
rk <48819681+rksan@users.noreply.github.com>
2022-12-26 16:42:24 +0900

    Enable "Vue.js devtool"

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack");
+
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   outputDir: "docs",
@@ -8,6 +10,12 @@ module.exports = defineConfig({
     plugins: [
       require("unplugin-vue-components/webpack")({
         /* options */
+      }),
+      // @see : https://stackoverflow.com/questions/64081026/how-use-bundler-build-feature-flags-in-vue-3-0-with-webpack
+      // Define Bundler Build Feature Flags
+      new webpack.DefinePlugin({
+        // Drop Options API from bundle
+        __VUE_OPTIONS_API__: true,
       }),
     ],
   },
